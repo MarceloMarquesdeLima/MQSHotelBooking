3668a0d442f8e7ccce07f4311c89d29322696fcb
Marcelo Marques <marcelomarqueslima.dev@outlook.com>
2024-04-12 04:49:35 -0300

    Inciando Geração do banco
    Criando as migration e conexão com banco

---
+++ MQSBookingService/Adapter/Adapter.SQL/MQSDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Entities = Domain.Entities;
+
+namespace Adapter.SQL
+{
+	public class MQSDbContext : DbContext
+	{
+		public MQSDbContext(DbContextOptions<MQSDbContext>options) : base(options)	{ }
+
+        /*protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Guest>();
+        }*/
+
+        public virtual DbSet<Entities.Guest> Guests { get; set; }
+	}
+}
+
--- MQSBookingService/Consumer/ApiConsumer/Program.cs
+++ MQSBookingService/Consumer/ApiConsumer/Program.cs
@@ -1,8 +1,18 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Adapter.SQL;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+//Conexão com banco
+var connectionString = builder.Configuration.GetConnectionString("Main");
+builder.Services.AddDbContext<MQSDbContext>(
+    options => options.UseSqlServer(connectionString, b => b.MigrationsAssembly(typeof(MQSDbContext).Assembly.FullName)));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
