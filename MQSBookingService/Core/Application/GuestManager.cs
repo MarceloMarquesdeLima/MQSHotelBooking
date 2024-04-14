888c4f184c04bb9bad75f8f292dd8209b6833a10
Marcelo Marques de Lima <111276703+MarceloMarquesdeLima@users.noreply.github.com>
2024-04-13 23:05:39 -0300

    git pacote

---
+++ MQSBookingService/Core/Application/GuestManager.cs
@@ -0,0 +1,30 @@
+﻿using Application.Guest.DTO;
+using Application.Guest.Ports;
+using Application.Guest.Requests;
+using Application.Guest.Responses;
+using Domain.Ports;
+
+namespace Application
+{
+    public class GuestManager : IGuestManager
+    {
+        private IGuestRepository _guestRepository;
+
+        public GuestManager(IGuestRepository guestRepository)
+        {
+            _guestRepository = guestRepository;
+        }
+
+        public async Task<GuestResponse> CreateGuest(CreateGuestRequest request)
+        {
+            var guest = GuestDTO.MapToEntity(request.Data);
+            request.Data.Id = await _guestRepository.Create(guest);
+
+            return new GuestResponse
+            {
+                Data = request.Data,
+                Sucess = true,
+            };
+        }
+    }
+}
--- MQSBookingService/Core/Domain/Ports/IGuestRepository.cs
+++ MQSBookingService/Core/Domain/Ports/IGuestRepository.cs
@@ -6,7 +6,7 @@ namespace Domain.Ports
 	public interface IGuestRepository
 	{
 		Task<Guest> Get(int Id);
-		Task<int> Save(Guest guest);
+		Task<int> Create(Guest guest);
 	}
 }
 
