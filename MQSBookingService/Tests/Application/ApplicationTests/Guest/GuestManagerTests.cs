20b17b1bd66d51be59d5fadc786fa49c6f9e510e
Marcelo Marques de Lima <111276703+MarceloMarquesdeLima@users.noreply.github.com>
2024-04-18 04:41:34 -0300

    Atualização projeeto

--- MQSBookingService/Tests/Application/ApplicationTests/Guest/GuestManagerTests.cs
+++ MQSBookingService/Tests/Application/ApplicationTests/Guest/GuestManagerTests.cs
@@ -1,16 +1,56 @@
-﻿using NUnit.Framework;
+﻿using Application;
+using Domain.Ports;
+using Guest = Domain.Entities.Guest;
+using NUnit.Framework;
+using Application.Guest.DTO;
+using Application.Guest.Requests;
 
-namespace ApplicationTests.Guest
+namespace ApplicationTests.Guests
 {
+    class FakeRepo : IGuestRepository
+    {
+        public Task<int> Create(Guest guest)
+        {
+            return Task.FromResult(1111);
+        }
+
+        public Task<Guest> Get(int Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
     public class GuestManagerTests
     {
+        GuestManager guestManager;
+
         [SetUp]
-        public void Setup() { }
+        public void Setup() 
+        {
+            var fakeRepo = new FakeRepo();
+            guestManager = new GuestManager(fakeRepo);
+        }
 
         [Test]
-        public void Test1()
+        public async Task Test1()
         {
-            Assert.Pass();
+            var guestDTO = new GuestDTO
+            {
+                Name = "Test",
+                Surname = "Testando",
+                Email = "testando@gmail.com",
+                IdNumber = "abcd",
+                IdTypeCode = 1
+            };
+
+            var request = new CreateGuestRequest()
+            {
+                Data = guestDTO,
+            };
+
+            var res = await guestManager.CreateGuest(request);
+            Assert.IsNotNull(res);
+            Assert.True(res.Success);
         }
     }
 }
