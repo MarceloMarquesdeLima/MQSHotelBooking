b451f9a0159ddc5f2268252b6cda9123b151818d
Marcelo Marques de Lima <111276703+MarceloMarquesdeLima@users.noreply.github.com>
2024-06-01 04:27:33 -0300

    Tests Booking Manager

---
+++ MQSBookingService/Tests/Application/ApplicationTests/Booking/BookingManagerTest.cs
@@ -0,0 +1,76 @@
+﻿using Application.Booking;
+using Application.Guest;
+using Application.Payment;
+using Application.Payment.DTOS;
+using Application.Payment.Ports;
+using Domain.Enums;
+using Domain.Ports;
+using Moq;
+using NUnit.Framework;
+using static Application.Booking.DTO.PaymentRequestDTO;
+
+namespace ApplicationTests.Booking
+{
+    public class BookingManagerTest
+    {
+        GuestManager guestManager;
+
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public async Task Should_PayForABooking()
+        {
+            var dto = new PaymentRequestDto
+            {
+                SelectedPaymentProvider = SupportedPaymentProviders.MercadoPago,
+                PaymentIntention = "https://www.mercadopago.com.br/asdf",
+                SelectedPaymentMethod = SupportedPaymentMethods.CreditCard
+            };
+
+            var bookingRepository = new Mock<IBookingRepository>();
+            var roomRepository = new Mock<IRoomRepository>();
+            var guestRepository = new Mock<IGuestRepository>();
+            var paymentProcessorFactory = new Mock<IPaymentProcessorFactory>();
+            var paymentProcessor = new Mock<IPaymentProcessor>();
+
+            var responseDto = new PaymentStateDTO
+            {
+                CreatedDate = DateTime.Now,
+                Message = $"Successfully paid {dto.PaymentIntention}",
+                PaymentId = "123",
+                Status = (Application.Payment.Enums.StatusEnum)Status.Success
+            };
+
+            var response = new PaymentResponse
+            {
+                Data = responseDto,
+                Success = true,
+                Message = "Payment successfully processed"
+            };
+
+            paymentProcessor.
+                Setup(x => x.CapturePayment(dto.PaymentIntention))
+                .Returns(Task.FromResult(response));
+
+            paymentProcessorFactory
+                .Setup(x => x.GetPaymentProcessor(dto.SelectedPaymentProvider))
+                .Returns(paymentProcessor.Object);
+
+            var bookingManager = new BookingManager(
+                bookingRepository.Object,
+                roomRepository.Object,
+                guestRepository.Object,
+                paymentProcessorFactory.Object);
+
+            var res = await bookingManager.PayForABooking(dto);
+
+            Assert.NotNull(res);
+            Assert.True(res.Success);
+            Assert.AreEqual(res.Message, "Payment successfully processed");
+        }
+    }
+}
