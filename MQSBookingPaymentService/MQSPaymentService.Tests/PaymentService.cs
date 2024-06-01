8b91b4e4fc68f86c80bb1d6ee1cd00981edf6342
Marcelo Marques de Lima <111276703+MarceloMarquesdeLima@users.noreply.github.com>
2024-06-01 04:43:11 -0300

    Criação dos tests de pagamentos

---
+++ MQSBookingPaymentService/MQSPaymentService.Tests/PaymentService.cs
@@ -0,0 +1,45 @@
+﻿using Application;
+using NUnit.Framework;
+using PaymentApplications.MercadoPago;
+using PaymentsApplication;
+using static Application.Booking.DTO.PaymentRequestDTO;
+
+namespace MQSPaymentService.Tests
+{
+    public class PaymentService
+    {
+        [Test]
+        public void ShouldReturn_NotImplementedPaymentProvider_WhenAskingForStripeProvider()
+        {
+            var factory = new PaymentProcessorFactory();
+
+            var provider = factory.GetPaymentProcessor(SupportedPaymentProviders.Stripe);
+
+            Assert.AreEqual(provider.GetType(), typeof(NotImplementedPaymentProvider));
+        }
+
+        [Test]
+        public void ShouldReturn_MercadoPagoAdapter_Provider()
+        {
+            var factory = new PaymentProcessorFactory();
+
+            var provider = factory.GetPaymentProcessor(SupportedPaymentProviders.MercadoPago);
+
+            Assert.AreEqual(provider.GetType(), typeof(MercadoPagoAdapter));
+        }
+
+        [Test]
+        public async Task ShouldReturnFalse_WhenCapturingPaymentFor_NotImplementedPaymentProvider()
+        {
+            var factory = new PaymentProcessorFactory();
+
+            var provider = factory.GetPaymentProcessor(SupportedPaymentProviders.Stripe);
+
+            var res = await provider.CapturePayment("https://myprovider.com/asdf");
+
+            Assert.False(res.Success);
+            Assert.AreEqual(res.ErrorCode, ErrorCodes.PAYMENT_PROVIDER_NOT_IMPLEMENTED);
+            Assert.AreEqual(res.Message, "The selected payment provider is not available at the moment");
+        }
+    }
+}
